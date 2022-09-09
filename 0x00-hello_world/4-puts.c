06b93dbc4d4eb39f063f7bb59a9b2664df6e5bbe
mcjoycoffie <mcjoycoffie@gmail.com>
2022-09-09 02:34:53 -0700

    Hello world with puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main -print the string in the put function
+ * main-print the string in the put function
  *
  * Description: using the main funtion
  * this program prints "Programming is like building a multilingual puzzle"
@@ -9,6 +9,6 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
