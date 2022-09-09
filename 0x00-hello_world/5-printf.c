9a9e56cbf50902811e2a66cd8fd1567c2815e5d5
mcjoycoffie <mcjoycoffie@gmail.com>
2022-09-09 05:03:18 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - print the string in the printf function
- * this program prints "with porper grammar, but the outcome is a piece of art\n"
+ * this program prints "with porper grammar, but the outcome is a piece of art"
  * Return: 0;
  */
 int main(void)
