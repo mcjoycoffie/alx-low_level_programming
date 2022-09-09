bdfa5add92f91b33cef54ae201839dc1e5fedb70
mcjoycoffie <mcjoycoffie@gmail.com>
2022-09-09 05:19:07 -0700

    Sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,27 @@
+include <stdio.h>
+
+/**
+ * main - print the string in put function
+ *
+ * Description: using the main function 
+ * this program prints "Programming is like multilingual puzzle"
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+
+	printf("Size of a char: %ld byte(s)\n" sizeof(c));
+	printf("Size of an int: %ld byte(s)\n" sizeof(i));
+	print("Size of a long in: %ld byte(s)\n" sizeof(li));
+	print("Size of a long long: %ld byte(s)\n" sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n" sizeof(f);
+	return (0);
+
+}
