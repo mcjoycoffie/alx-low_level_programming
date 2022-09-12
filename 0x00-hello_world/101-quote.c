caf51f2ddc20dd838d59c4ff6a77a80a8883df1e
mcjoycoffie <mcjoycoffie@gmail.com>
2022-09-12 12:32:35 -0700

    print the string

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	\n
+	printf("and that piece of art is useful" "\n Dora Korpar, 2015-10-19");
+	
 	return(1);
 
 
