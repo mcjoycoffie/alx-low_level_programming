08adb711d8ac8ce11d05c32cba7021af19c3f93c
mcjoycoffie <mcjoycoffie@gmail.com>
2022-09-12 12:30:34 -0700

    print the string

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *
+ *Writing a program that prints a string
+ *
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	\n
+	return(1);
+
+
+
+
+
+}
