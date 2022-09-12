eaecb8957aa49f233f9046b226e4cf408e7ae0dc
mcjoycoffie <mcjoycoffie@gmail.com>
2022-09-12 00:26:26 -0700

    Size Of Data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
-include <stdio.h>
+#include <stdio.h>
+
 
 /**
  * main - print the string in put function
@@ -10,18 +11,11 @@ include <stdio.h>
 
 int main(void)
 {
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
-
-
-	printf("Size of a char: %ld byte(s)\n" sizeof(c));
-	printf("Size of an int: %ld byte(s)\n" sizeof(i));
-	print("Size of a long in: %ld byte(s)\n" sizeof(li));
-	print("Size of a long long: %ld byte(s)\n" sizeof(lli));
-	printf("Size of a float: %ld byte(s)\n" sizeof(f));
+	printf("Size of a char: %ld byte(s)\n" sizeof(char));
+	printf("Size of an int: %ld byte(s)\n" sizeof(int));
+	print("Size of a long in: %ld byte(s)\n" sizeof(long int));
+	print("Size of a long long: %ld byte(s)\n" sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n" sizeof(float));
 	return (0);
 
 }
